5a0aceb6f5f12b37bcd86966113a73e2328ef7e9
Luan Bianchini <luanbianchine1@gmail.com>
2023-10-31 17:23:23 -0300

    refactor: specify the controller function

--- src/presentation/controllers/add.movie.controller.ts
+++ src/presentation/controllers/add.movie.controller.ts
@@ -3,13 +3,13 @@ import { type HttpResponse } from '../../@types/http'
 import { type CreateMovie } from '../../data/usecases/create.movie'
 import { type MovieProps } from '../../domain/entities/movie'
 
-export class MovieController implements Controller<MovieProps> {
+export class AddMovieController implements Controller<MovieProps> {
   constructor (private readonly create: CreateMovie) {}
 
   public async handle (input: MovieProps): Promise<HttpResponse> {
     const res = await this.create.perform(input)
     return {
-      statusCode: 200,
+      statusCode: 201,
       body: res
     }
   }

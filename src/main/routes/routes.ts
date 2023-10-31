a608c2c81c9c32d379e7364c886a10135768294b
Luan Bianchini <luanbianchine1@gmail.com>
2023-10-31 17:29:45 -0300

    feat: create route

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { type Router } from 'express'
+import makeRoute from '../adapters/express.adapter'
+import { makeAddController } from '../factories/controller.factory'
 
 export default (router: Router): void => {
-  router.get('/api/home', (req, res) => {
-    res.send('oi')
-  })
+  router.get('/api/home', makeRoute(makeAddController()))
 }

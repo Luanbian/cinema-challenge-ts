9b378c3284733dcc73b7e132f32272ded07ac259
Luan Bianchini <luanbianchine1@gmail.com>
2023-10-31 16:39:10 -0300

    feat: created movie entity

---
+++ src/domain/entities/movie.ts
@@ -0,0 +1,28 @@
+import { randomUUID } from 'crypto'
+
+export interface MovieProps {
+  name: string
+  synopsis: string
+  releaseDate: Date
+  inTheaters: boolean
+}
+
+export class Movie {
+  private readonly id: string
+  private readonly name: string
+  private readonly synopsis: string
+  private readonly releaseDate: Date
+  private readonly inTheaters: boolean
+
+  private constructor (props: MovieProps) {
+    this.id = randomUUID()
+    this.name = props.name
+    this.synopsis = props.synopsis
+    this.releaseDate = props.releaseDate
+    this.inTheaters = props.inTheaters
+  }
+
+  public static create (input: MovieProps): Movie {
+    return new Movie(input)
+  }
+}

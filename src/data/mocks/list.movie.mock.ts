ce9d10a541b996134ff4bf783566b0451c93d549
Luan Bianchini <luanbianchine1@gmail.com>
2023-11-01 16:24:49 -0300

    refactor: move mock to specific local

---
+++ src/data/mocks/list.movie.mock.ts
@@ -0,0 +1,35 @@
+import { type Movie } from '../../domain/entities/movie'
+import { type queryParamns } from '../../presentation/controllers/find.all.movie.controller'
+import { type IlistMovie } from '../protocols/list.movie.protocol'
+
+export const makeListMock = (): IlistMovie => {
+  class ListMock implements IlistMovie {
+    public async perform (paramns: queryParamns): Promise<{
+      result: Movie[]
+      length: number
+      hasMore: boolean
+    }> {
+      return {
+        result: [
+          {
+            id: 'any_id_1',
+            name: 'any_name_1',
+            synopsis: 'any_synopsis_1',
+            releaseDate: new Date('01/11/2023'),
+            inTheaters: true
+          },
+          {
+            id: 'any_id_2',
+            name: 'any_name_2',
+            synopsis: 'any_synopsis_2',
+            releaseDate: new Date('02/11/2023'),
+            inTheaters: false
+          }
+        ],
+        length: 2,
+        hasMore: false
+      }
+    }
+  }
+  return new ListMock()
+}
--- src/presentation/controllers/tests/find.all.movie.controller.spec.ts
+++ src/presentation/controllers/tests/find.all.movie.controller.spec.ts
@@ -1,43 +1,12 @@
+import { makeListMock } from '../../../data/mocks/list.movie.mock'
 import { type IlistMovie } from '../../../data/protocols/list.movie.protocol'
-import { type Movie } from '../../../domain/entities/movie'
-import { FindAllMovieController, type queryParamns } from '../find.all.movie.controller'
+import { FindAllMovieController } from '../find.all.movie.controller'
 
 interface SutTypes {
   sut: FindAllMovieController
   list: IlistMovie
 }
 
-const makeListMock = (): IlistMovie => {
-  class ListMock implements IlistMovie {
-    public async perform (paramns: queryParamns): Promise<{
-      result: Movie[]
-      length: number
-      hasMore: boolean
-    }> {
-      return {
-        result: [
-          {
-            id: 'any_id_1',
-            name: 'any_name_1',
-            synopsis: 'any_synopsis_1',
-            releaseDate: new Date('01/11/2023'),
-            inTheaters: true
-          },
-          {
-            id: 'any_id_2',
-            name: 'any_name_2',
-            synopsis: 'any_synopsis_2',
-            releaseDate: new Date('02/11/2023'),
-            inTheaters: false
-          }
-        ],
-        length: 2,
-        hasMore: false
-      }
-    }
-  }
-  return new ListMock()
-}
 const makeSut = (): SutTypes => {
   const list = makeListMock()
   const sut = new FindAllMovieController(list)

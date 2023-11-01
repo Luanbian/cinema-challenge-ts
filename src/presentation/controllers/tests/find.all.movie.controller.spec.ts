474b4ccd2cd6a4c8e2a227597b3f629faf494d0f
Luan Bianchini <luanbianchine1@gmail.com>
2023-11-01 16:14:50 -0300

    test: return 200 if consult occurs ok

---
+++ src/presentation/controllers/tests/find.all.movie.controller.spec.ts
@@ -0,0 +1,71 @@
+import { type IlistMovie } from '../../../data/protocols/list.movie.protocol'
+import { type Movie } from '../../../domain/entities/movie'
+import { FindAllMovieController, type queryParamns } from '../find.all.movie.controller'
+
+interface SutTypes {
+  sut: FindAllMovieController
+}
+
+const makeListMock = (): IlistMovie => {
+  class ListMock implements IlistMovie {
+    public async perform (paramns: queryParamns): Promise<{
+      result: Movie[]
+      length: number
+      hasMore: boolean
+    }> {
+      return {
+        result: [
+          {
+            id: 'any_id_1',
+            name: 'any_name_1',
+            synopsis: 'any_synopsis_1',
+            releaseDate: new Date('01/11/2023'),
+            inTheaters: true
+          },
+          {
+            id: 'any_id_2',
+            name: 'any_name_2',
+            synopsis: 'any_synopsis_2',
+            releaseDate: new Date('02/11/2023'),
+            inTheaters: false
+          }
+        ],
+        length: 2,
+        hasMore: false
+      }
+    }
+  }
+  return new ListMock()
+}
+const makeSut = (): SutTypes => {
+  const list = makeListMock()
+  const sut = new FindAllMovieController(list)
+  return { sut }
+}
+
+describe('FindAllMovieController', () => {
+  test('should return status 200 if call list sucessfully', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({ column: 'name', type: 'asc', limit: '100', page: '1' })
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      content: [{
+        id: 'any_id_1',
+        inTheaters: true,
+        name: 'any_name_1',
+        releaseDate: new Date('01/11/2023'),
+        synopsis: 'any_synopsis_1'
+      }, {
+        id: 'any_id_2',
+        inTheaters: false,
+        name: 'any_name_2',
+        releaseDate: new Date('02/11/2023'),
+        synopsis: 'any_synopsis_2'
+      }],
+      hasMore: false,
+      length: 2,
+      limit: '100',
+      page: '1'
+    })
+  })
+})

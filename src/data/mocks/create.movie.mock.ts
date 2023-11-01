f741e726b6b717f8b722cc74e36985ce16da8366
Luan Bianchini <luanbianchine1@gmail.com>
2023-11-01 16:01:12 -0300

    refactor: create specific local to mocks

---
+++ src/data/mocks/create.movie.mock.ts
@@ -0,0 +1,19 @@
+import { type Movie } from '../../domain/entities/movie'
+import { type IcreateMovie } from '../protocols/create.movie.protocol'
+import { type movieDto } from '../usecases/create.movie'
+
+export const makeCreateMock = (): IcreateMovie => {
+  class CreateMovieMock implements IcreateMovie {
+    public async perform (props: movieDto): Promise<Movie> {
+      const fakeMovie: Movie = {
+        id: 'fake_id',
+        name: 'fake_name',
+        synopsis: 'fake_synopsis',
+        releaseDate: new Date('01/11/2023'),
+        inTheaters: false
+      }
+      return fakeMovie
+    }
+  }
+  return new CreateMovieMock()
+}
--- src/presentation/controllers/tests/add.movie.controller.spec.ts
+++ src/presentation/controllers/tests/add.movie.controller.spec.ts
@@ -1,27 +1,11 @@
+import { makeCreateMock } from '../../../data/mocks/create.movie.mock'
 import { type IcreateMovie } from '../../../data/protocols/create.movie.protocol'
 import { type movieDto } from '../../../data/usecases/create.movie'
-import { type Movie } from '../../../domain/entities/movie'
 import { AddMovieController } from '../add.movie.controller'
 
 interface SutTypes {
   sut: AddMovieController
   createMovieMock: IcreateMovie
-}
-
-const makeCreateMock = (): IcreateMovie => {
-  class CreateMovieMock implements IcreateMovie {
-    public async perform (props: movieDto): Promise<Movie> {
-      const fakeMovie: Movie = {
-        id: 'fake_id',
-        name: 'fake_name',
-        synopsis: 'fake_synopsis',
-        releaseDate: new Date('01/11/2023'),
-        inTheaters: false
-      }
-      return fakeMovie
-    }
-  }
-  return new CreateMovieMock()
 }
 
 const makeSut = (): SutTypes => {

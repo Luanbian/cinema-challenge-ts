787e538fb5bbaf9ad3a06d24f4875e54faff70d7
Luan Bianchini <luanbianchine1@gmail.com>
2023-11-14 22:59:50 -0300

    refactor: controller mock to express adapter test

--- src/main/tests/express.adapter.spec.ts
+++ src/main/tests/express.adapter.spec.ts
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/consistent-type-assertions */
 import makeRoute from '../adapters/express.adapter'
 import { type Request, type Response } from 'express'
-import { FindAllMovieController } from '../../presentation/controllers/find.all.movie.controller'
-import { makeListMock } from '../../data/mocks/list.movie.mock'
 
 describe('Express Adapter', () => {
   it('Deve lidar com erros', async () => {
@@ -11,12 +9,7 @@ describe('Express Adapter', () => {
       status: jest.fn().mockReturnThis(),
       json: jest.fn()
     } as unknown as Response
-    const listmock = makeListMock()
-    const controller = new FindAllMovieController(listmock)
-    jest.spyOn(controller, 'handle').mockImplementation(async () => {
-      throw new Error('Internal server error')
-    })
-    const route = makeRoute(controller)
+    const route = makeRoute({ async handle () { throw new Error() } })
     await route(mockRequest, mockResponse)
     expect(mockResponse.status).toHaveBeenCalledWith(500)
     expect(mockResponse.json).toHaveBeenCalledWith('Internal server error')

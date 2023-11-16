b5ecda4a7238beb471ca4c09f0b25dbb5e943cbc
Luan Bianchini <luanbianchine1@gmail.com>
2023-11-16 14:55:23 -0300

    refactor: make function to avoid repeat code

--- src/main/tests/express.adapter.spec.ts
+++ src/main/tests/express.adapter.spec.ts
@@ -2,27 +2,31 @@
 import makeRoute from '../adapters/express.adapter'
 import { type Request, type Response } from 'express'
 
+const makeHttpMock = (): { req: Request, res: Response } => {
+  const mockRequest = {} as Request
+  const mockResponse = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn()
+  } as unknown as Response
+  return {
+    req: mockRequest,
+    res: mockResponse
+  }
+}
+
 describe('Express Adapter', () => {
   test('should return 200 if success', async () => {
-    const mockRequest = {} as Request
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn()
-    } as unknown as Response
+    const { req, res } = makeHttpMock()
     const route = makeRoute({ async handle () { return { statusCode: 200, body: 'sucesso' } } })
-    await route(mockRequest, mockResponse)
-    expect(mockResponse.status).toHaveBeenCalledWith(200)
-    expect(mockResponse.json).toHaveBeenCalledWith('sucesso')
+    await route(req, res)
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.json).toHaveBeenCalledWith('sucesso')
   })
   test('should return 500 if throws an error', async () => {
-    const mockRequest = {} as Request
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn()
-    } as unknown as Response
+    const { req, res } = makeHttpMock()
     const route = makeRoute({ async handle () { throw new Error() } })
-    await route(mockRequest, mockResponse)
-    expect(mockResponse.status).toHaveBeenCalledWith(500)
-    expect(mockResponse.json).toHaveBeenCalledWith('Internal server error')
+    await route(req, res)
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.json).toHaveBeenCalledWith('Internal server error')
   })
 })

dbdb17604093a34b88ca7385acaf506fac546b67
Luan Bianchini <luanbianchine1@gmail.com>
2023-11-16 17:35:37 -0300

    test: pass route.get test with new index.ts

--- src/main/tests/route.get.api.movies.spec.ts
+++ src/main/tests/route.get.api.movies.spec.ts
@@ -1,18 +1,13 @@
 import supertest from 'supertest'
-import { app } from '../index'
-import { type Request, type Response } from 'express'
+import appServer from '../index'
 
+const app = appServer.getApp()
 describe('GET /api/movies', () => {
+  afterAll(() => {
+    appServer.close()
+  })
   test('should return statusCode 200 if consult is ok', async () => {
     const response = await supertest(app).get('/api/movies')
     expect(response.statusCode).toBe(200)
-  })
-  test('should return 500 if route throws', async () => {
-    (app as any).get('/api/data', (req: Request, res: Response) => {
-      res.status(500).json({ error: 'Internal server error' })
-    })
-    const response = await supertest(app).get('/api/data')
-    expect(response.status).toBe(500)
-    expect(response.body).toEqual({ error: 'Internal server error' })
   })
 })

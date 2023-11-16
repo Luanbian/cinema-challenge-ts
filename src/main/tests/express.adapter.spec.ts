5ef33df0ed5255338b387d83e6a5367c99f0e386
Luan Bianchini <luanbianchine1@gmail.com>
2023-11-16 14:50:45 -0300

    test: express adapter return 200

--- src/main/tests/express.adapter.spec.ts
+++ src/main/tests/express.adapter.spec.ts
@@ -3,7 +3,18 @@ import makeRoute from '../adapters/express.adapter'
 import { type Request, type Response } from 'express'
 
 describe('Express Adapter', () => {
-  it('Deve lidar com erros', async () => {
+  test('should return 200 if success', async () => {
+    const mockRequest = {} as Request
+    const mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    } as unknown as Response
+    const route = makeRoute({ async handle () { return { statusCode: 200, body: 'sucesso' } } })
+    await route(mockRequest, mockResponse)
+    expect(mockResponse.status).toHaveBeenCalledWith(200)
+    expect(mockResponse.json).toHaveBeenCalledWith('sucesso')
+  })
+  test('should return 500 if throws an error', async () => {
     const mockRequest = {} as Request
     const mockResponse = {
       status: jest.fn().mockReturnThis(),

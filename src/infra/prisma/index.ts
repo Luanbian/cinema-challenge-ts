2bf686c70ee956e5de22f695b1d18f21d1cad117
Luan Bianchini <luanbianchine1@gmail.com>
2023-11-17 16:14:47 -0300

    feat: add dotenv cli to fix prisma

--- src/infra/prisma/index.ts
+++ src/infra/prisma/index.ts
@@ -1,13 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import * as dotenv from 'dotenv'
-dotenv.config()
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.NODE_ENV === process.env.STATUS ? process.env.DATABASE_URL_TEST : process.env.DATABASE_URL
-    }
-  }
-})
+const prisma = new PrismaClient()
 
 export default prisma

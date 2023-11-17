728a1c66fa3131a1bf151396911713256ea736cc
Luan Bianchini <luanbianchine1@gmail.com>
2023-11-17 12:59:26 -0300

    feat: configure prisma to integration test

--- src/infra/prisma/index.ts
+++ src/infra/prisma/index.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from '@prisma/client'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.NODE_ENV === process.env.STATUS ? process.env.DATABASE_URL_TEST : process.env.DATABASE_URL
+    }
+  }
+})
 
 export default prisma

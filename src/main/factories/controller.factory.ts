01b1efbee24812724aa89bec512a537dcfd7b42c
luan <luanbianchine1@gmail.com>
2023-11-24 18:25:27 -0300

    feat: create find all employer controller factory

--- src/main/factories/controller.factory.ts
+++ src/main/factories/controller.factory.ts
@@ -1,6 +1,7 @@
 import { type Controller } from '../../@types/controller'
 import { type movieDto } from '../../data/usecases/create.movie'
 import { AddMovieController } from '../../presentation/controllers/add.movie.controller'
+import { FindAllEmployerController } from '../../presentation/controllers/find.all.employer.controller'
 import { FindAllMovieController, type queryParamns } from '../../presentation/controllers/find.all.movie.controller'
 import { makeCreateMovie, makeListMovie } from './usecase.factory'
 
@@ -12,4 +13,8 @@ export const makeAddController = (): Controller<movieDto> => {
 export const makeFindAllController = (): Controller<queryParamns> => {
   const list = makeListMovie()
   return new FindAllMovieController(list)
+}
+
+export const makeFindAllEmployerController = (): Controller<null> => {
+  return new FindAllEmployerController()
 }

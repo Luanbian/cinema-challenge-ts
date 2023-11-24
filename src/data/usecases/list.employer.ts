8f5537b27159b98e53de9434bf8e82d820fcc025
luan <luanbianchine1@gmail.com>
2023-11-24 19:47:36 -0300

    fix: enum problem

--- src/data/usecases/list.employer.ts
+++ src/data/usecases/list.employer.ts
@@ -1,4 +1,4 @@
-import { type Employer } from '../../domain/entities/employer'
+import { type Employer, Roles } from '../../domain/entities/employer'
 import { type IfindAllEmployers } from '../../infra/protocols/find.all.employers.protocols'
 import { type IlistEmployer } from '../protocols/list.employer.protocol'
 
@@ -7,6 +7,28 @@ export class ListEmployer implements IlistEmployer {
 
   public async perform (): Promise<{ result: Employer[], length: number }> {
     const { result, length } = await this.repository.findAll()
-    return { result, length }
+
+    const mappedResult = result.map((employer) => ({
+      ...employer,
+      role: this.mapPrismaRolesToAppRoles(employer.role)
+    }))
+
+    return { result: mappedResult, length }
+  }
+
+  private mapPrismaRolesToAppRoles (role: string): Roles {
+    const roleMap: Record<string, Roles> = {
+      ADMIN: Roles.ADMIN,
+      CADASTRER: Roles.CADASTRER,
+      MANAGER: Roles.MANAGER,
+      CONSULTER: Roles.CONSULTER,
+      TRAINEE: Roles.TRAINEE
+    }
+
+    if (role in roleMap) {
+      return roleMap[role]
+    }
+
+    throw new Error(`Unexpected role: ${role}`)
   }
 }

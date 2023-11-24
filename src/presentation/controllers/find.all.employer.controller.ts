ef9959e4aaf425203f09ce5ae5ccbcf264a8e872
luan <luanbianchine1@gmail.com>
2023-11-24 18:41:30 -0300

    feat: inject usecase in controller and  complete controller

--- src/presentation/controllers/find.all.employer.controller.ts
+++ src/presentation/controllers/find.all.employer.controller.ts
@@ -1,12 +1,23 @@
 import { type Controller } from '../../@types/controller'
 import { type HttpResponse } from '../../@types/http'
-import { ok, serverError } from '../helpers/http.helper'
+import { type IlistEmployer } from '../../data/protocols/list.employer.protocol'
+import { noContent, ok, serverError } from '../helpers/http.helper'
 
 export class FindAllEmployerController implements Controller<null> {
+  constructor (private readonly list: IlistEmployer) {}
+
   public async handle (): Promise<HttpResponse> {
     try {
-      return ok('tudo certo')
+      const res = await this.list.perform()
+      if (res.length === 0) return noContent()
+      const body = {
+        length: res.length,
+        hasMore: res.hasMore,
+        content: res.result
+      }
+      return ok(body)
     } catch (error) {
+      console.error(error)
       return serverError(error)
     }
   }

42779560b72c965bc20922b9fb4fe6a92e578d38
luan <luanbianchine1@gmail.com>
2023-11-24 18:29:00 -0300

    feat: create route to employer controller

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { type Router } from 'express'
 import makeRoute from '../adapters/express.adapter'
-import { makeAddMoviesController, makeFindAllMoviesController } from '../factories/controller.factory'
+import { makeAddMoviesController, makeFindAllEmployersController, makeFindAllMoviesController } from '../factories/controller.factory'
 import { handleMovieValidation, validateMovieInput } from '../../middleware/validator'
 
 export default (router: Router): void => {
   router.post('/api/movies', validateMovieInput, handleMovieValidation, makeRoute(makeAddMoviesController()))
   router.get('/api/movies', makeRoute(makeFindAllMoviesController()))
+
+  router.get('/api/employers', makeRoute(makeFindAllEmployersController()))
 }

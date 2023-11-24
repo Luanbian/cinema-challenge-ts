0ee156edaec761ae2e0da44208aed17050836736
luan <luanbianchine1@gmail.com>
2023-11-24 19:05:19 -0300

    feat: create employer entity

--- src/data/usecases/list.employer.ts
+++ src/data/usecases/list.employer.ts
@@ -1,8 +1,10 @@
+import { type Employer } from '../../domain/entities/employer'
 import { type IfindAllEmployers } from '../../infra/protocols/find.all.employers.protocols'
 import { type IlistEmployer } from '../protocols/list.employer.protocol'
 
 export class ListEmployer implements IlistEmployer {
   constructor (private readonly repository: IfindAllEmployers) {}
+
   public async perform (): Promise<{ result: Employer[], length: number, hasMore: boolean }> {
 
   }
---
+++ src/domain/entities/employer.ts
@@ -0,0 +1,35 @@
+export interface EmployerProps {
+  id: string
+  name: string
+  email: string
+  password: string
+  role: Roles
+}
+
+export enum Roles {
+  ADMIN,
+  CADASTRER,
+  MANAGER,
+  CONSULTER,
+  TRAINEE
+}
+
+export class Employer {
+  readonly id: string
+  readonly name: string
+  readonly email: string
+  readonly password: string
+  readonly role: Roles
+
+  private constructor (props: EmployerProps) {
+    this.id = props.id
+    this.name = props.name
+    this.email = props.email
+    this.password = props.password
+    this.role = props.role
+  }
+
+  public static create (input: EmployerProps): Employer {
+    return new Employer(input)
+  }
+}

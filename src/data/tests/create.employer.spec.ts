85717a8e197a7f5eda342c1bd915f08baa4ff85f
luan <luanbianchine1@gmail.com>
2023-11-25 13:59:30 -0300

    test: usecase sae employer tested

--- src/data/tests/create.employer.spec.ts
+++ src/data/tests/create.employer.spec.ts
@@ -1,5 +1,5 @@
 import { randomUUID } from 'crypto'
-import { Employer } from '../../domain/entities/employer'
+import { Employer, Roles } from '../../domain/entities/employer'
 import { type EmployerDto, type IcreateEmployer } from '../protocols/create.employer.protocol'
 
 interface SutProps {
@@ -36,6 +36,20 @@ const makeSut = (): SutProps => {
 
 describe('CreateEmployer', () => {
   test('should return a employer entity', async () => {
-
+    const { sut } = makeSut()
+    const props: EmployerDto = {
+      name: 'employer_name',
+      email: 'employer@email',
+      password: '****',
+      role: Roles.CADASTRER
+    }
+    const employer = await sut.perform(props)
+    expect(employer).toEqual({
+      id: employer.id,
+      name: 'employer_name',
+      email: 'employer@email',
+      password: '****',
+      role: Roles.CADASTRER
+    })
   })
 })

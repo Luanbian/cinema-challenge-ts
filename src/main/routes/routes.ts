3867d18c025d5c90e2d3f2b0b760c833bf4c7360
luan <luanbianchine1@gmail.com>
2023-11-25 14:32:27 -0300

    feat: add route with validation

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { type Router } from 'express'
 import makeRoute from '../adapters/express.adapter'
-import { makeAddMoviesController, makeFindAllEmployersController, makeFindAllMoviesController } from '../factories/controller.factory'
-import { handleValidation, validateMovieInput } from '../../middleware/validator'
+import { makeAddEmployerController, makeAddMoviesController, makeFindAllEmployersController, makeFindAllMoviesController } from '../factories/controller.factory'
+import { handleValidation, validateEmployerInput, validateMovieInput } from '../../middleware/validator'
 
 export default (router: Router): void => {
   router.post('/api/movies', validateMovieInput, handleValidation, makeRoute(makeAddMoviesController()))
   router.get('/api/movies', makeRoute(makeFindAllMoviesController()))
 
   router.get('/api/employers', makeRoute(makeFindAllEmployersController()))
+  router.post('/api/employers', validateEmployerInput, handleValidation, makeRoute(makeAddEmployerController()))
 }

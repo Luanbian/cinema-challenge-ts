7bea2b78b76eb79cbec5cddb1224ce9ca9e9163e
luan <luanbianchine1@gmail.com>
2023-11-25 13:40:49 -0300

    test: controller tested

--- src/presentation/tests/add.employer.controller.spec.ts
+++ src/presentation/tests/add.employer.controller.spec.ts
@@ -1,5 +1,6 @@
 import { makeCreateEmployerMock } from '../../data/mocks/create.employer.mock'
-import { type IcreateEmployer } from '../../data/protocols/create.employer.protocol'
+import { type EmployerDto, type IcreateEmployer } from '../../data/protocols/create.employer.protocol'
+import { Roles } from '../../domain/entities/employer'
 import { AddEmployerController } from '../controllers/add.employer.controller'
 
 interface SutProps {
@@ -15,6 +16,37 @@ const makeSut = (): SutProps => {
 
 describe('AddEmployerController', () => {
   test('should return status code 201 and employer entity if success', async () => {
-
+    const { sut } = makeSut()
+    const body: EmployerDto = {
+      name: 'test',
+      email: 'test@email.com',
+      password: '****',
+      role: Roles.ADMIN
+    }
+    const httpResponse = await sut.handle(body)
+    expect(httpResponse.statusCode).toBe(201)
+    expect(httpResponse.body).toEqual({
+      id: 'employer_id_test',
+      name: 'employer_name_test',
+      email: 'employer_email_test',
+      password: 'employer_pass_test',
+      role: Roles.ADMIN
+    })
+  })
+  test('should return status code 500 if controller throws', async () => {
+    const { sut, create } = makeSut()
+    jest.spyOn(create, 'perform').mockImplementationOnce(async () => {
+      return await new Promise((resolve, reject) => {
+        reject(new Error())
+      })
+    })
+    const body: EmployerDto = {
+      name: 'test',
+      email: 'test@email.com',
+      password: '****',
+      role: Roles.ADMIN
+    }
+    const httpResponse = await sut.handle(body)
+    expect(httpResponse.statusCode).toBe(500)
   })
 })

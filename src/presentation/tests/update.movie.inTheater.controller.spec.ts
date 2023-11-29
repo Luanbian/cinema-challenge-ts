0b7425acbafd6026c8408e2cd87dfd9296470ea6
Luan Bianchini <luanbianchine1@gmail.com>
2023-11-29 12:10:12 -0300

    test: UpdateMovieInTheaterController

---
+++ src/presentation/tests/update.movie.inTheater.controller.spec.ts
@@ -0,0 +1,56 @@
+import { type IupdateMovieInTheater } from '../../data/protocols/update.movie.inTheater.protocol'
+import { UpdateMovieInTheaterController, type idParam } from '../controllers/update.movie.inTheater.controller'
+
+interface SutProps {
+  sut: UpdateMovieInTheaterController
+  update: IupdateMovieInTheater
+}
+
+export const makeUpdate = (): IupdateMovieInTheater => {
+  class UpdateMovieInTheater implements IupdateMovieInTheater {
+    public async perform ({ id }: idParam): Promise<string | null> {
+      return `updated item ${id}`
+    }
+  }
+  return new UpdateMovieInTheater()
+}
+
+const makeSut = (): SutProps => {
+  const update = makeUpdate()
+  const sut = new UpdateMovieInTheaterController(update)
+  return { sut, update }
+}
+
+describe('UpdateMovieInTheaterController', () => {
+  test('should return status code 200 and string if success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({ id: 'valid_id_movie' })
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toBe('updated item valid_id_movie')
+  })
+  test('should call usecase with correct value', async () => {
+    const { update } = makeSut()
+    const id = 'movie_id'
+    const usecase = await update.perform({ id })
+    expect(usecase).toEqual(`updated item ${id}`)
+  })
+  test('should return status code 500 if controller throws', async () => {
+    const { sut, update } = makeSut()
+    jest.spyOn(update, 'perform').mockImplementationOnce(async () => {
+      return await new Promise((resolve, reject) => {
+        reject(new Error())
+      })
+    })
+    const httpResponse = await sut.handle({ id: 'movie_id' })
+    expect(httpResponse.statusCode).toBe(500)
+  })
+  test('should return status code 404 if id not found', async () => {
+    const { sut, update } = makeSut()
+    jest.spyOn(update, 'perform').mockImplementationOnce(async () => {
+      return null
+    })
+    const httpResponse = await sut.handle({ id: 'invalid_id_movie' })
+    expect(httpResponse.statusCode).toBe(404)
+    expect(httpResponse.body).toEqual('id not found')
+  })
+})

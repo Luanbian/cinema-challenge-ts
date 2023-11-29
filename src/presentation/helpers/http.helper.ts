addd9aaa2415dd62b25aafa1326a07167c9123a7
Luan Bianchini <luanbianchine1@gmail.com>
2023-11-29 12:08:57 -0300

    feat: add not found in http helper

--- src/presentation/helpers/http.helper.ts
+++ src/presentation/helpers/http.helper.ts
@@ -10,4 +10,5 @@ function httpRes<T> (statusCode: number, body: T): HttpResponse {
 export const created = <T>(data: T): HttpResponse => httpRes(201, data)
 export const serverError = <T>(data: T): HttpResponse => httpRes(500, data)
 export const noContent = (): HttpResponse => httpRes(204, null)
+export const notFound = <T>(data: T): HttpResponse => httpRes(404, data)
 export const ok = <T>(data: T): HttpResponse => httpRes(200, data)

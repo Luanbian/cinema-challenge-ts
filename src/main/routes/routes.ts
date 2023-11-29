41796ca15c31138db750c923a52343e4abc6147c
Luan Bianchini <luanbianchine1@gmail.com>
2023-11-29 14:16:19 -0300

    feat: create put route

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { type Router } from 'express'
 import makeRoute from '../adapters/express.adapter'
-import { makeAddEmployerController, makeAddMoviesController, makeFindAllEmployersController, makeFindAllMoviesController } from '../factories/controller.factory'
+import { makeAddEmployerController, makeAddMoviesController, makeFindAllEmployersController, makeFindAllMoviesController, makeUpdateMovieInTheaterController } from '../factories/controller.factory'
 import { handleValidation, validateEmployerInput, validateMovieInput } from '../../middleware/validator'
 
 export default (router: Router): void => {
   router.post('/api/movies', validateMovieInput, handleValidation, makeRoute(makeAddMoviesController()))
   router.get('/api/movies', makeRoute(makeFindAllMoviesController()))
+  router.put('/api/movies/:id', makeRoute(makeUpdateMovieInTheaterController()))
 
   router.get('/api/employers', makeRoute(makeFindAllEmployersController()))
   router.post('/api/employers', validateEmployerInput, handleValidation, makeRoute(makeAddEmployerController()))

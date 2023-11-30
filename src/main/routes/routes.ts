937904c4f31f66a5d554cf08294fa715a757aac0
Luan Bianchini <luanbianchine1@gmail.com>
2023-11-30 12:14:54 -0300

    feat: create login route with validation

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { type Router } from 'express'
 import makeRoute from '../adapters/express.adapter'
-import { makeAddEmployerController, makeAddMoviesController, makeFindAllEmployersController, makeFindAllMoviesController, makeUpdateMovieInTheaterController } from '../factories/controller.factory'
-import { handleValidation, validateEmployerInput, validateMovieInput } from '../../middleware/validator'
+import { makeAddEmployerController, makeAddMoviesController, makeFindAllEmployersController, makeFindAllMoviesController, makeLoginEmployerController, makeUpdateMovieInTheaterController } from '../factories/controller.factory'
+import { handleValidation, validateEmployerInput, validateLoginInput, validateMovieInput } from '../../middleware/validator'
 
 export default (router: Router): void => {
   router.post('/api/movies', validateMovieInput, handleValidation, makeRoute(makeAddMoviesController()))
@@ -11,4 +11,6 @@ export default (router: Router): void => {
 
   router.get('/api/employers', makeRoute(makeFindAllEmployersController()))
   router.post('/api/employers', validateEmployerInput, handleValidation, makeRoute(makeAddEmployerController()))
+
+  router.post('/api/login', validateLoginInput, handleValidation, makeRoute(makeLoginEmployerController()))
 }
--- src/middleware/validator.ts
+++ src/middleware/validator.ts
@@ -1,15 +1,7 @@
 import { type Request, type Response, type NextFunction } from 'express'
 import { body, validationResult } from 'express-validator'
 
-const validateMovieInput = [
-  body('name').isString(),
-  body('synopsis').isString(),
-  body('releaseDate').isString(),
-  body('inTheaters').isBoolean()
-]
-
 const roles = ['admin', 'cadastrer', 'manager', 'consulter', 'trainee']
-
 const validateEmployerInput = [
   body('name').isString(),
   body('email').isEmail(),
@@ -17,7 +9,19 @@ const validateEmployerInput = [
   body('role').isIn(roles).withMessage('Invalid role')
 ]
 
-export { validateMovieInput, validateEmployerInput }
+const validateMovieInput = [
+  body('name').isString(),
+  body('synopsis').isString(),
+  body('releaseDate').isString(),
+  body('inTheaters').isBoolean()
+]
+
+const validateLoginInput = [
+  body('email').isEmail(),
+  body('password').isString()
+]
+
+export { validateMovieInput, validateEmployerInput, validateLoginInput }
 
 export function handleValidation (req: Request, res: Response, next: NextFunction): void {
   const errors = validationResult(req)

7dbe42fabd1f6abde656144beebbf03ac664655a
Luan Bianchini <luanbianchine1@gmail.com>
2023-11-30 12:02:36 -0300

    test: usecase login tested

--- src/data/tests/login.employer.spec.ts
+++ src/data/tests/login.employer.spec.ts
@@ -43,7 +43,35 @@ const makeSut = (): SutTypes => {
 }
 
 describe('LoginEmployer', () => {
-  test('should', async () => {
-    //
+  test('should call repository with correct values', async () => {
+    const { sut, repository } = makeSut()
+    const repoSpy = jest.spyOn(repository, 'findUserByAuth')
+    const auth: Iauth = {
+      email: 'valid_email',
+      password: '1234'
+    }
+    await sut.perform(auth)
+    expect(repoSpy).toHaveBeenLastCalledWith(auth)
+  })
+  test('should return token if user finded with success', async () => {
+    const { sut } = makeSut()
+    const auth: Iauth = {
+      email: 'valid_email',
+      password: '1234'
+    }
+    const usecase = await sut.perform(auth)
+    expect(usecase).toBe('token')
+  })
+  test('should return null if user not be found', async () => {
+    const { sut, repository } = makeSut()
+    jest.spyOn(repository, 'findUserByAuth').mockImplementationOnce(async () => {
+      return null
+    })
+    const auth: Iauth = {
+      email: 'invalid_email',
+      password: '1234'
+    }
+    const usecase = await sut.perform(auth)
+    expect(usecase).toBe(null)
   })
 })

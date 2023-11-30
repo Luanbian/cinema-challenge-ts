6e7f90a4343b8c8f750b0be638cea90015ca7c99
Luan Bianchini <luanbianchine1@gmail.com>
2023-11-30 11:34:29 -0300

    test: controller use cases tested

--- src/presentation/tests/login.employer.controller.spec.ts
+++ src/presentation/tests/login.employer.controller.spec.ts
@@ -27,14 +27,20 @@ export class LoginEmployerController {
   public async handle (auth: Iauth): Promise<{ statusCode: number, body: any }> {
     try {
       const res = await this.login.perform(auth)
+      if (res === null) {
+        return {
+          statusCode: 401,
+          body: 'unauthorized'
+        }
+      }
       return {
         statusCode: 200,
         body: res
       }
     } catch (error) {
       return {
-        statusCode: 401,
-        body: 'Unauthorized'
+        statusCode: 500,
+        body: 'server error'
       }
     }
   }
@@ -51,10 +57,33 @@ describe('LoginEmployerController', () => {
     const { sut, login } = makeSut()
     const loginSpy = jest.spyOn(login, 'perform')
     const auth: Iauth = {
-      email: 'teste.email@gmail.com',
+      email: 'valid_email',
       password: '1234'
     }
     await sut.handle(auth)
     expect(loginSpy).toHaveBeenCalledWith(auth)
+  })
+  test('should return status code 200 if user autorized with success', async () => {
+    const { sut } = makeSut()
+    const auth: Iauth = {
+      email: 'valid_email',
+      password: '1234'
+    }
+    const httpResponse = await sut.handle(auth)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toBe('autorized')
+  })
+  test('should return status code 401 if user not autorized', async () => {
+    const { sut, login } = makeSut()
+    jest.spyOn(login, 'perform').mockImplementationOnce(async () => {
+      return null
+    })
+    const auth: Iauth = {
+      email: 'invalid_email',
+      password: '1234'
+    }
+    const httpResponse = await sut.handle(auth)
+    expect(httpResponse.statusCode).toBe(401)
+    expect(httpResponse.body).toBe('unauthorized')
   })
 })

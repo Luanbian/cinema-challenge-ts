4282cf7d7f7aeebd40951773a842b9a4af0196f6
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-01 10:00:12 -0300

    feat: add new jwt method protocol

--- src/data/protocols/authenticate.protocol.ts
+++ src/data/protocols/authenticate.protocol.ts
@@ -1,5 +1,7 @@
+import { type JwtPayload } from 'jsonwebtoken'
 import { type Employer } from '../../domain/entities/employer'
 
 export interface Authenticate {
   generateToken: (user: Employer) => Promise<string>
+  verifyToken: (token: string) => Promise<string | JwtPayload>
 }

54a751c3e96f56bd96b9453cb6fbefeec8ba5a16
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-01 15:43:21 -0300

    feat: put auth in routes

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -3,14 +3,17 @@ import { type Router } from 'express'
 import makeRoute from '../adapters/express.adapter'
 import { makeAddEmployerController, makeAddMoviesController, makeFindAllEmployersController, makeFindAllMoviesController, makeLoginEmployerController, makeUpdateMovieInTheaterController } from '../factories/controller.factory'
 import { handleValidation, validateEmployerInput, validateLoginInput, validateMovieInput } from '../../middleware/validator'
+import makeAuthMiddleware from '../../middleware/auth/auth.middleware'
+import { makeAuthenticateAdapter } from '../factories/adapter.factory'
 
+const auth = makeAuthMiddleware(makeAuthenticateAdapter())
 export default (router: Router): void => {
-  router.post('/api/movies', validateMovieInput, handleValidation, makeRoute(makeAddMoviesController()))
-  router.get('/api/movies', makeRoute(makeFindAllMoviesController()))
-  router.put('/api/movies/:id', makeRoute(makeUpdateMovieInTheaterController()))
+  router.post('/api/movies', auth, validateMovieInput, handleValidation, makeRoute(makeAddMoviesController()))
+  router.get('/api/movies', auth, makeRoute(makeFindAllMoviesController()))
+  router.put('/api/movies/:id', auth, makeRoute(makeUpdateMovieInTheaterController()))
 
-  router.get('/api/employers', makeRoute(makeFindAllEmployersController()))
-  router.post('/api/employers', validateEmployerInput, handleValidation, makeRoute(makeAddEmployerController()))
+  router.get('/api/employers', auth, makeRoute(makeFindAllEmployersController()))
+  router.post('/api/employers', auth, validateEmployerInput, handleValidation, makeRoute(makeAddEmployerController()))
 
   router.post('/api/login', validateLoginInput, handleValidation, makeRoute(makeLoginEmployerController()))
 }

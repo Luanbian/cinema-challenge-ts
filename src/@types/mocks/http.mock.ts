5975d8d09eec6f7b8733487a73b6f3dc2f1e1b50
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-01 12:19:33 -0300

    feat: add next in http mock

--- src/@types/mocks/http.mock.ts
+++ src/@types/mocks/http.mock.ts
@@ -1,14 +1,16 @@
 /* eslint-disable @typescript-eslint/consistent-type-assertions */
-import { type Request, type Response } from 'express'
+import { type NextFunction, type Request, type Response } from 'express'
 
-export const makeHttpMock = (): { req: Request, res: Response } => {
+export const makeHttpMock = (): { req: Request, res: Response, next: NextFunction } => {
   const mockRequest = {} as Request
   const mockResponse = {
     status: jest.fn().mockReturnThis(),
     json: jest.fn()
   } as unknown as Response
+  const mockNextFunction = jest.fn() as NextFunction
   return {
     req: mockRequest,
-    res: mockResponse
+    res: mockResponse,
+    next: mockNextFunction
   }
 }

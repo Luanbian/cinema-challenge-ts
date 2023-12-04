8bb649227666a652d74722a599f04546cff2478c
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-04 11:25:02 -0300

    fix: test to route login to seed credentials

--- src/main/tests/router.post.api.login.test.ts
+++ src/main/tests/router.post.api.login.test.ts
@@ -8,15 +8,15 @@ describe('POST /api/login', () => {
   })
   test('should return token if user loged with success', async () => {
     const response = await supertest(app).post('/api/login').send({
-      email: 'test@email.com',
-      password: 'any_pass'
+      email: 'seed@test.com',
+      password: '123'
     })
     expect(response.statusCode).toBe(200)
     expect(typeof response.body === 'string').toBeTruthy()
   })
   test('should return "senha incorreta" and status code 401 if password not match', async () => {
     const response = await supertest(app).post('/api/login').send({
-      email: 'test@email.com',
+      email: 'seed@test.com',
       password: 'invalid_password'
     })
     expect(response.statusCode).toBe(401)
@@ -25,7 +25,7 @@ describe('POST /api/login', () => {
   test('should return "usuário não encontrado" and status code 401 if user not found', async () => {
     const response = await supertest(app).post('/api/login').send({
       email: 'invalid_email@gmail.com',
-      password: 'any_pass'
+      password: '123'
     })
     expect(response.statusCode).toBe(401)
     expect(response.body).toBe('usuário não encontrado')
@@ -33,7 +33,7 @@ describe('POST /api/login', () => {
   test('should return bad request status code 400 if email was in invalid format', async () => {
     const response = await supertest(app).post('/api/login').send({
       email: 'invalid_email',
-      password: 'any_pass'
+      password: '123'
     })
     expect(response.statusCode).toBe(400)
     expect(response.body).toEqual({
@@ -50,7 +50,7 @@ describe('POST /api/login', () => {
   })
   test('should return bad request status code 400 if password was number', async () => {
     const response = await supertest(app).post('/api/login').send({
-      email: 'test@email.com',
+      email: 'seed@test.com',
       password: 1234
     })
     expect(response.statusCode).toBe(400)
@@ -68,7 +68,7 @@ describe('POST /api/login', () => {
   })
   test('should return bad request status code 400 if password was boolean', async () => {
     const response = await supertest(app).post('/api/login').send({
-      email: 'test@email.com',
+      email: 'seed@test.com',
       password: true
     })
     expect(response.statusCode).toBe(400)

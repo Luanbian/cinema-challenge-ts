a058b8094906ca8620a497109e6538db48e5a092
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-04 14:16:41 -0300

    test: ensure deps are called with correct values

--- src/data/tests/login.employer.spec.ts
+++ src/data/tests/login.employer.spec.ts
@@ -1,16 +1,17 @@
 import { makeFindUserByAuthStub } from '../../infra/mocks/find.user.by.auth.mock'
 import { type IfindUserByAuth } from '../../infra/protocols/find.user.by.auth.protocol'
-import { type LoginEmployerControllerProps } from '../../presentation/controllers/login.employer.controller'
 import { makeAuthenticateStub } from '../mocks/authenticate.mock'
 import { makeEncrypterStub } from '../mocks/encrypter.mock'
 import { type Encrypter } from '../criptography/protocol/encrypter.protocol'
-import { type Ilogin } from '../protocols/login.employer.protocol'
+import { type Iauth, type Ilogin } from '../protocols/login.employer.protocol'
 import { LoginEmployer } from '../usecases/login.employer'
+import { type Authenticate } from '../../middleware/auth/protocol/authenticate.protocol'
 
 interface SutTypes {
   repository: IfindUserByAuth
   sut: Ilogin
   bcryptStub: Encrypter
+  jwtStub: Authenticate
 }
 
 const makeSut = (): SutTypes => {
@@ -18,23 +19,45 @@ const makeSut = (): SutTypes => {
   const bcryptStub = makeEncrypterStub()
   const jwtStub = makeAuthenticateStub()
   const sut = new LoginEmployer(repository, bcryptStub, jwtStub)
-  return { sut, repository, bcryptStub }
+  return { sut, repository, bcryptStub, jwtStub }
 }
 
 describe('LoginEmployer', () => {
   test('should call repository with correct values', async () => {
     const { sut, repository } = makeSut()
     const repoSpy = jest.spyOn(repository, 'findUserByAuth')
-    const auth: LoginEmployerControllerProps = {
+    const auth: Iauth = {
       email: 'valid_email',
-      password: '1234'
+      password: 'valid_password'
     }
     await sut.perform(auth)
     expect(repoSpy).toHaveBeenLastCalledWith(auth.email)
   })
+  test('should call bcrypt with correct values', async () => {
+    const { sut, bcryptStub, repository } = makeSut()
+    const bcryptSpy = jest.spyOn(bcryptStub, 'matchPassword')
+    const auth: Iauth = {
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const user = await repository.findUserByAuth(auth.email)
+    await sut.perform(auth)
+    expect(bcryptSpy).toHaveBeenCalledWith(auth.password, user?.password)
+  })
+  test('should call jwt with correct values', async () => {
+    const { sut, jwtStub, repository } = makeSut()
+    const jwtSpy = jest.spyOn(jwtStub, 'generateToken')
+    const auth: Iauth = {
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const user = await repository.findUserByAuth(auth.email)
+    await sut.perform(auth)
+    expect(jwtSpy).toHaveBeenCalledWith(user)
+  })
   test('should return token if user finded and password match with success', async () => {
     const { sut } = makeSut()
-    const auth: LoginEmployerControllerProps = {
+    const auth: Iauth = {
       email: 'valid_email',
       password: 'valid_password'
     }
@@ -46,9 +69,9 @@ describe('LoginEmployer', () => {
     jest.spyOn(repository, 'findUserByAuth').mockImplementationOnce(async () => {
       return null
     })
-    const auth: LoginEmployerControllerProps = {
+    const auth: Iauth = {
       email: 'invalid_email',
-      password: '1234'
+      password: 'valid_password'
     }
     const promise = sut.perform(auth)
     await expect(promise).rejects.toThrow('usuário não encontrado')
@@ -58,7 +81,7 @@ describe('LoginEmployer', () => {
     jest.spyOn(bcryptStub, 'matchPassword').mockImplementationOnce(async () => {
       return false
     })
-    const auth: LoginEmployerControllerProps = {
+    const auth: Iauth = {
       email: 'invalid_email',
       password: 'invalid_password'
     }

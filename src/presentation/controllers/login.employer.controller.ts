b56d09dff2f2d21d1280d1dc628e496a74e98ee9
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-05 16:50:55 -0300

    fix: condfition unauthorized in login controller

--- src/presentation/controllers/login.employer.controller.ts
+++ src/presentation/controllers/login.employer.controller.ts
@@ -2,7 +2,8 @@ import { type Controller } from '../../@types/controller'
 import { type HttpResponse } from '../../@types/http'
 import { type Iauth, type Ilogin } from '../../data/protocols/login.employer.protocol'
 import { makeLog } from '../../main/factories/adapter.factory'
-import { ok, serverError } from '../helpers/http.helper'
+import { ExpectedError } from '../helpers/expected.error'
+import { ok, serverError, unauthorized } from '../helpers/http.helper'
 
 export interface LoginEmployerControllerProps {
   email: string
@@ -22,6 +23,10 @@ export class LoginEmployerController implements Controller <LoginEmployerControl
       await makeLog().execute('info', 'user logged', { user: { email: auth.email } })
       return ok(res)
     } catch (error) {
+      if (error instanceof ExpectedError) {
+        await makeLog().execute('error', 'user wrorn credentials', { credentials: paramns }, new Error(error.message))
+        return unauthorized(error.message)
+      }
       await makeLog().execute('crit', 'server error', 'login controller throws', new Error(error))
       return serverError(error)
     }

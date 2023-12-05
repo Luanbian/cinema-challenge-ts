2d9b0b4362274c2f0e90e6dafcf6ccd4bc338266
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-05 11:56:41 -0300

    feat: configure log

--- logs/adapter/log.adapter.ts
+++ logs/adapter/log.adapter.ts
@@ -3,6 +3,6 @@ import { type types, type Ilogs } from '../protocol/log.adapter.protocol'
 
 export class LogAdapter implements Ilogs {
   public async execute (type: types, msg: string): Promise<void> {
-    logger[type]({ msg })
+    logger[type](msg)
   }
 }
--- logs/logger.ts
+++ logs/logger.ts
@@ -1,7 +1,14 @@
 import path from 'path'
-import winstom from 'winston'
+import winstom, { format } from 'winston'
 
 const logger = winstom.createLogger({
+  format: format.combine(
+    format.timestamp({
+      format: 'DD-MM-YYYY hh:mm:ss A'
+    }),
+    format.prettyPrint(),
+    format.printf((log) => `[${log.timestamp}] ${log.level}: ${log.message}`)
+  ),
   transports: [
     new winstom.transports.File({
       filename: path.join(__dirname, './reports/output_app.log')

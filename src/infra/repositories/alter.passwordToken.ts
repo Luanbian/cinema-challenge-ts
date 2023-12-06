6c57934ac8786c9717f28f069df2b85c0f6148cc
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-06 14:17:35 -0300

    feat: create concret reset repository

---
+++ src/infra/repositories/alter.passwordToken.ts
@@ -0,0 +1,16 @@
+import prisma from '../prisma'
+import { type IupdatePasswordToken, type IupdatePasswordTokenProps } from '../protocols/update.passwordToken.protocol'
+
+export class AlterPasswordToken implements IupdatePasswordToken {
+  public async alterPassToken (paramns: IupdatePasswordTokenProps): Promise<void> {
+    await prisma.employers.update({
+      data: {
+        passwordToken: paramns.token,
+        passwordTokenExpires: paramns.expiresAt
+      },
+      where: {
+        id: paramns.id
+      }
+    })
+  }
+}
